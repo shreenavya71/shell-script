357dedbebc24b272988111eb56c22d68b2db1f4c
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-04-18 19:49:25 +0100

    new commit in variable.sh

---
+++ 03.variablesintro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#variable declaration
+PERSON1=navya
+PERSON2=sai
+
+#referring the variable
+echo "$PERSON1:: hello £PERSON2, how are you?"
+echo "£PERSON2:: hi $PERSON1 i am fine how are you?"
+echo "$PERSON1:: i am fine too, thanks for asking. how is your work going?"
+echo "£PERSON2:: not bad, i am thinking to upgrade to devops"
