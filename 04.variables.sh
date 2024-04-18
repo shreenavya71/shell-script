76cb616cd018068dba7d10711a765883729558c1
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-04-18 20:08:39 +0100

    new commit in variable.sh

---
+++ 04.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#variable declaration   passing the variable values outside the script is called arguments/parameters
+PERSON1=$3
+PERSON2=$2
+
+#referring the variable
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1 i am fine how are you?"
+echo "$PERSON1:: i am fine too, thanks for asking. how is your work going?"
+echo "$PERSON2:: not bad, i am thinking to upgrade to devops"
