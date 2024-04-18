cf2ecd0080759b0b4ee0cc605642737a0f678b65
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-04-18 19:32:39 +0100

    new commit in variable.sh

---
+++ variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "navya:: hello sai, how are you?"
+echo "sai:: i am fine how are you?"
+echo "navya:: i am fine too, thanks for asking. how is your work going?"
+echo "sai:: not bad, i am thinking to upgrade to devops"
