1f3b849a6cc6cb8f1c0622a6217dc5417a93a407
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-04-18 20:26:10 +0100

    new commit in variable.sh

---
+++ 05.variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter username::"
+read USERNAME   # here USERNAME is a variable
+
+echo "please enter the password::"
+read PASSWORD 
+
+#PRINTING THE VALUES OF THE USERNAME AND PASSWORD
+echo "username is: $USERNAME, password is: $PASSWORD"
