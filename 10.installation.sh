eda337518a2f20a6fcaa45636a9d5ff7ca11cc44
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-05-02 14:16:57 +0100

    new commit

--- 10.installation.sh
+++ 10.installation.sh
@@ -2,9 +2,28 @@
 
 
 USERID=$(id -u)
-if [$USERID -ne 0]
+
+if [ $USERID -ne 0 ]
 then 
     echo "please run this script with root access."
 else
     echo "you are super user"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql-----n FAILURE"
+    exit 1
+else
+    echo "installation of mysql------SUCCESS"
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of git ----------FAILURE"
+    exit 1
+else
+    echo "installation of git  ---------- SUCESS"
