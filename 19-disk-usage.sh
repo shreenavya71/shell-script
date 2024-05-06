216e7441d28ed2904ff2082ee043b071b52a8b57
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-05-06 17:01:56 +0100

    new fun commit

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
 
-while IFS read -r line            #IFS internal field separator, using line
+while IFS= read -r line            #IFS internal field separator, using line
 do 
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )    #6--6th row
     FOLDER=$(echo $line | awk -F " " '{print $NF}')        #N---last
@@ -11,4 +11,4 @@ do
     then
          echo "$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
     fi
-do <<< $DISK_USAGE
+done <<< $DISK_USAGE
