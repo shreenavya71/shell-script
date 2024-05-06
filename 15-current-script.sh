41487fb48e3fb63fce384b4035d2b40b86fdd815
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-05-06 09:25:23 +0100

    new fun commit

---
+++ 15-current-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script is : $$"
+
+./16-other-script.sh
+
+# source ./16-other-script.sh
+
+echo "After calling other script, course: $COURSE"
+
